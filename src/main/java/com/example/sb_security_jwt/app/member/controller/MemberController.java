95b9ab781ce9d11a428f6bbeec9ba8ad932cd80d
somang-lim <somangjuha@gmail.com>
2022-10-13 10:56:16 +0900

    TEST_2: POST /member/login 으로 올바른 username과 password 데이터를 넘기면 JWT 키를 발급해준다.

--- src/main/java/com/example/sb_security_jwt/app/member/controller/MemberController.java
+++ src/main/java/com/example/sb_security_jwt/app/member/controller/MemberController.java
@@ -1,20 +1,25 @@
 package com.example.sb_security_jwt.app.member.controller;
 
 import lombok.Data;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletResponse;
-
 @RestController
 @RequestMapping("/member")
 public class MemberController {
     @PostMapping("/login")
-    public String login(@RequestBody LoginDto loginDto, HttpServletResponse response) {
-        response.addHeader("Authentication", "JWT토큰");
-        return "username : %s, password : %s".formatted(loginDto.getUsername(), loginDto.getPassword());
+    public ResponseEntity<String> login(@RequestBody LoginDto loginDto) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authentication", "JWT키");
+
+        String body = "username : %s, password : %s".formatted(loginDto.getUsername(), loginDto.getPassword());
+
+        return new ResponseEntity<>(body, headers, HttpStatus.OK);
     }
 
     @Data
