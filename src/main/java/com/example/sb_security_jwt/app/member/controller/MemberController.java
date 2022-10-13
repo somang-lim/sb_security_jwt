f812488ca0fdc27cb2c8fdb6f25987272220bbbf
somang-lim <somangjuha@gmail.com>
2022-10-13 14:05:32 +0900

    로그인과 회원가입 엔드 포인트를 제외하고, 전부 인증필요, 필터 추가

--- src/main/java/com/example/sb_security_jwt/app/member/controller/MemberController.java
+++ src/main/java/com/example/sb_security_jwt/app/member/controller/MemberController.java
@@ -32,7 +32,7 @@ public class MemberController {
 
     @GetMapping("/me")
     public ResponseEntity<RsData> me(@AuthenticationPrincipal MemberContext memberContext) {
-        if (memberContext == null) {
+        if (memberContext == null) { // 임시코드, 나중에는 시프링 시큐리티를 이용해서 로그인을 안했다면, 아예 여기로 못 들어오도록
             return Util.spring.responseEntityOf(RsData.failOf(null));
         }
 
