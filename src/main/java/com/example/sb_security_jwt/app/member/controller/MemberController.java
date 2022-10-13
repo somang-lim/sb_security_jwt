b2a1435401758469c316a92e18294afbfd6ea62e
somang-lim <somangjuha@gmail.com>
2022-10-13 10:51:03 +0900

    TEST_1: POST /member/login 으로 올바른 username과 password 데이터를 넘기면 JWT 키를 발급해준다.

--- src/main/java/com/example/sb_security_jwt/app/member/controller/MemberController.java
+++ src/main/java/com/example/sb_security_jwt/app/member/controller/MemberController.java
@@ -1,18 +1,19 @@
 package com.example.sb_security_jwt.app.member.controller;
 
-import com.example.sb_security_jwt.app.member.service.MemberService;
 import lombok.Data;
-import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
+
 @RestController
 @RequestMapping("/member")
 public class MemberController {
     @PostMapping("/login")
-    public String login(@RequestBody LoginDto loginDto) {
+    public String login(@RequestBody LoginDto loginDto, HttpServletResponse response) {
+        response.addHeader("Authentication", "JWT토큰");
         return "username : %s, password : %s".formatted(loginDto.getUsername(), loginDto.getPassword());
     }
 
--- src/test/java/com/example/sb_security_jwt/AuthTests.java
+++ src/test/java/com/example/sb_security_jwt/AuthTests.java
@@ -6,14 +6,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.nio.charset.StandardCharsets;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -46,6 +49,34 @@ public class AuthTests {
         // Then
         resultActions
                 .andExpect(status().is2xxSuccessful());
+    }
+
+    @Test
+    @DisplayName("POST /member/login 으로 올바른 username과 password 데이터를 넘기면 JWT 키를 발급해준다.")
+    void t2() throws Exception {
+        // When
+        ResultActions resultActions = mvc
+                .perform(post("/member/login")
+                        .content("""
+                                {
+                                    "username": "user1",
+                                    "password": "1234"
+                                }
+                                """.stripIndent())
+                        .contentType(new MediaType(APPLICATION_JSON, UTF_8))
+                )
+                .andDo(print());
+
+        resultActions
+                .andExpect(status().is2xxSuccessful());
+
+        MvcResult mvcResult = resultActions.andReturn();
+
+        MockHttpServletResponse response = mvcResult.getResponse();
+
+        String authentication = response.getHeader("Authentication");
+
+        assertThat(authentication).isNotEmpty();
     }
 
 }
