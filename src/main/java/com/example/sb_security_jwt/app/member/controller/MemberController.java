e92bd5d594cbf3b96333741969299bef3bf8fbb7
somang-lim <somangjuha@gmail.com>
2022-10-13 11:17:35 +0900

    RsData 도입하여 HTTP 응답에 항상 resultCode, msg, data가 포함되도록 강제

--- src/main/java/com/example/sb_security_jwt/app/member/controller/MemberController.java
+++ src/main/java/com/example/sb_security_jwt/app/member/controller/MemberController.java
@@ -1,7 +1,9 @@
 package com.example.sb_security_jwt.app.member.controller;
 
+import com.example.sb_security_jwt.app.base.dto.RsData;
 import com.example.sb_security_jwt.app.member.entity.Member;
 import com.example.sb_security_jwt.app.member.service.MemberService;
+import com.example.sb_security_jwt.app.util.Util;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
@@ -21,27 +23,25 @@ public class MemberController {
     private final PasswordEncoder passwordEncoder;
 
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody LoginDto loginDto) {
+    public ResponseEntity<RsData> login(@RequestBody LoginDto loginDto) {
         if (loginDto.isNotValid()) {
-            return new ResponseEntity<>(null, null, HttpStatus.BAD_REQUEST);
+            return Util.spring.responseEntityOf(RsData.of("F-1", "로그인 정보가 올바르지 않습니다."));
         }
 
         Member member = memberService.findByUsername(loginDto.getUsername()).orElse(null);
 
         if (member == null) {
-            return new ResponseEntity<>(null, null, HttpStatus.BAD_REQUEST);
+            return Util.spring.responseEntityOf(RsData.of("F-2", "일치하는 회원이 존재하지 않습니다."));
         }
 
         if (passwordEncoder.matches(loginDto.getPassword(), member.getPassword()) == false) {
-            return new ResponseEntity<>(null, null, HttpStatus.BAD_REQUEST);
+            return Util.spring.responseEntityOf(RsData.of("F-3", "비밀번호가 일치하지 않습니다."));
         }
 
         HttpHeaders headers = new HttpHeaders();
-        headers.set("Authentication", "JWT키");
+        headers.set("Authentication", "JWT_Access_Token");
 
-        String body = "username : %s, password : %s".formatted(loginDto.getUsername(), loginDto.getPassword());
-
-        return new ResponseEntity<>(body, headers, HttpStatus.OK);
+        return Util.spring.responseEntityOf(RsData.of("S-1", "로그인 성공, Access Token을 발급합니다."), headers);
     }
 
     @Data
---
+++ src/main/java/com/example/sb_security_jwt/app/util/Util.java
@@ -0,0 +1,18 @@
+package com.example.sb_security_jwt.app.util;
+
+import com.example.sb_security_jwt.app.base.dto.RsData;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+public class Util {
+    public static class spring {
+        public static <T> ResponseEntity<RsData> responseEntityOf(RsData<T> rsData) {
+            return responseEntityOf(rsData, null);
+        }
+
+        public static <T> ResponseEntity<RsData> responseEntityOf(RsData<T> rsData, HttpHeaders headers) {
+            return new ResponseEntity<>(rsData, headers, rsData.isSuccess() ? HttpStatus.OK : HttpStatus.BAD_REQUEST);
+        }
+    }
+}
