c3df607d8130e513d476fd9d0201f37529401fcf
somang-lim <somangjuha@gmail.com>
2022-10-13 09:29:31 +0900

    setting

---
+++ src/main/java/com/example/sb_security_jwt/SbSecurityJwtApplication.java
@@ -0,0 +1,13 @@
+package com.example.sb_security_jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbSecurityJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbSecurityJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sb_security_jwt/SbSecurityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sb_security_jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbSecurityJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
