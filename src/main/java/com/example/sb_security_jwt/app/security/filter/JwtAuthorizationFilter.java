fe86590800be22d7041643aee25f878b1bb678f2
somang-lim <somangjuha@gmail.com>
2022-10-13 15:10:48 +0900

    필터에서 요청헤더의 Authorization 필드에 토큰값이 유효한지 확인, MemberContext 객체 생성, 스프링 시큐리티에 등록

--- src/main/java/com/example/sb_security_jwt/app/security/filter/JwtAuthorizationFilter.java
+++ src/main/java/com/example/sb_security_jwt/app/security/filter/JwtAuthorizationFilter.java
@@ -1,6 +1,15 @@
 package com.example.sb_security_jwt.app.security.filter;
 
+import com.example.sb_security_jwt.app.member.entity.Member;
+import com.example.sb_security_jwt.app.member.service.MemberService;
+import com.example.sb_security_jwt.app.security.entity.MemberContext;
+import com.example.sb_security_jwt.app.security.jwt.JwtProvider;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -9,13 +18,48 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class JwtAuthorizationFilter extends OncePerRequestFilter {
+    private final JwtProvider jwtProvider;
+    private final MemberService memberService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        log.debug("JwtAuthorizationFilter 실행됨");
+        String bearerToken = request.getHeader("Authorization");
+
+        if (bearerToken != null) {
+            String token = bearerToken.substring("Bearer ".length());
+
+            if (jwtProvider.verify(token)) {
+                Map<String, Object> claims = jwtProvider.getClaims(token);
+                String username = (String) claims.get("username");
+                Member member = memberService.findByUsername(username).orElseThrow(
+                        () -> new UsernameNotFoundException("'%s' Username not found.".formatted(username))
+                );
+
+                forceAuthentication(member);
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private void forceAuthentication(Member member) {
+        MemberContext memberContext = new MemberContext(member);
+
+        UsernamePasswordAuthenticationToken authenticationToken =
+                UsernamePasswordAuthenticationToken.authenticated(
+                        memberContext,
+                        null,
+                        member.getAuthorities()
+                );
+
+        SecurityContext context = SecurityContextHolder.createEmptyContext();
+        context.setAuthentication(authenticationToken);
+        SecurityContextHolder.setContext(context);
     }
 }
